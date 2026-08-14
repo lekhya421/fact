409d9d89c71dad36bb252cd44bb50852122ca37e
lekhya <lekhya.gopireddy@gmail.com>
2026-08-14 14:30:23 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -11,7 +11,7 @@ public class Factorial {
     }
 
     public static void main(String[] args) {
-        int number = 5;
+        int number = 4;
         System.out.println("Factorial of " + number + " is " + compute(number));
     }
 }
