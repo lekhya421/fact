076302754689ed51ee2bd6c11ce13079a5cb4cd8
lekhya.gopireddy@gmail.com <lekhya.gopireddy@gmail.com>
2026-08-14 14:03:32 +0530

    Fix factorial main method

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,8 @@
 public class Factorial {
     public static long compute(int n) {
-        if (n < 0) throw new IllegalArgumentException("Negative number not allowed");
+        if (n < 0)
+            throw new IllegalArgumentException("Negative number not allowed");
+
         long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
@@ -8,8 +10,8 @@ public class Factorial {
         return result;
     }
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         int number = 5;
         System.out.println("Factorial of " + number + " is " + compute(number));
-    }*/
+    }
 }
